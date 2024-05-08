d5b6957ece3cb3f676ebe10b57205adabe6b0078
Daniel <n11545020@qut.edu.au>
2024-05-08 14:48:43 +1000

    added commenting to explain how the event view controller works to display events from DB

--- src/main/java/com/example/groupupcab302/EventViewController.java
+++ src/main/java/com/example/groupupcab302/EventViewController.java
@@ -26,6 +26,8 @@ public class EventViewController implements Initializable {
     private EventDAO eventDAO = new EventDAO();
     private List<Event> eventList;
 
+    // Before dynamically rendering events, fetch all available events from DB
+    // Store them to a field for accessing
     private void initializeEventList() throws SQLException{
             eventList = eventDAO.getAllEvents();
     }
@@ -46,6 +48,15 @@ public class EventViewController implements Initializable {
     @FXML
     public Label eventLocation1, eventLocation2, eventLocation3, eventLocation4;
 
+    /* Summary of how events are dynamically rendered /TLDR
+        For Each Event:
+        Load the event card layout from the FXML file.
+        Update the content of the event card with data from the current event retrieved from the database.
+        Add the event card to the GridPane, positioning it according to the current grid layout (using the columns and rows variables).
+        Grid pane allows a row and column variable to be taken upon trying to add content to the pane. This means we can define where to position the event card
+        Set margins around the event card to provide spacing between cards within the GridPane.
+        Repeat for the number of events within the DB
+    */
      @Override
     public void initialize(URL location, ResourceBundle resources) {
          try {
@@ -55,20 +66,29 @@ public class EventViewController implements Initializable {
              initializeEventList();
 
              for (int counter = 0; counter<eventList.size() ; counter++){
+                 // Create an fxml loader object
                  FXMLLoader fxmlLoader = new FXMLLoader();
+                 // Use the fxml loader object to load the event card fxml doc to retrieve the structure
                  fxmlLoader.setLocation(getClass().getResource("event-cards.fxml"));
 
+                 // Create a vbox to hold the structure returned by the event card fxml file upon load
                  VBox eventCardPlacementBox = fxmlLoader.load();
 
+                 // Retrieve the controller associated with the event card fxml
                  EventCardController eventCardController = fxmlLoader.getController();
+                 // Set the content of the card by supplying the data of an event retrieved from the database
                  eventCardController.setData(eventList.get(counter));
 
+                 // Check that the number of event cards created does not exceed 3 per row
                  if (columns == 3){
+                     // Reset the number to 0 to start placing cards at the start of the row
                      columns = 0;
+                     // Increment the rows to move down 1 before starting to place the next event cards
                      ++rows;
                  }
-
+                 // Add the event card to the actual grid pane for display
                  eventGrid.add(eventCardPlacementBox, columns++, rows);
+                 // Create a margin between all event cards of 10 pixels for all sides
                  GridPane.setMargin(eventCardPlacementBox, new Insets(10));
              }
 
